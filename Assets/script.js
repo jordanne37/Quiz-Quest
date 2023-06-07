d6ccb379a4d0ad9bf4d253af7f3371983c02ba53
JordAnne <jbethune37@gmail.com>
2023-06-06 22:11:40 -0400

    added calls for my script file and some fun emojis to html file

---
+++ Assets/script.js
@@ -0,0 +1,20 @@
+// calls for program game
+var timerEl = document.getElementById("timer");
+var startDiv = document.getElementById("start");
+var questionEl = document.getElementById("question");
+var questionDiv = document.getElementById("question-div");
+var answerEl = document.getElementById("answer");
+var startEl = document.getElementById("start-btn");
+var scoreEl = document.getElementById("view-scores");
+var finalScoreEl = document.getElementById("final-score");
+var initialsEl = document.getElementById("initials");
+var gameEndEl = document.getElementById("game-over");
+
+// after game calls
+var highScoreBtnEl = document.getElementById("view-scores-btn");
+var clearScoreBtnEl = document.getElementById("clear-scores-btn");
+var submitScoreBtnEl = document.getElementById("submit-score-btn");
+var displayInitEl = document.getElementById("display-initials");
+var displayScoreEl = document.getElementById("display-score");
+var highScoreLink = document.getElementById("view-scores-link");
+var mainEl = document.getElementById("main-screen");
