cb4d5eb4be665e100c3fdb389793b31c648c7746
JordAnne <jbethune37@gmail.com>
2023-06-07 21:45:31 -0400

    added questions

--- Assets/script.js
+++ Assets/script.js
@@ -18,3 +18,54 @@ var displayInitEl = document.getElementById("display-initials");
 var displayScoreEl = document.getElementById("display-score");
 var highScoreLink = document.getElementById("view-scores-link");
 var mainEl = document.getElementById("main-screen");
+
+
+
+
+
+
+// questions
+function generateQuestion() {
+    if (questionNumber === 1) {
+      questionEl.textContent = "1. String values must be enclosed within ______ when being assigned to variables.";
+      answerSet = answer1.map(value => ({ value, sort: Math.random() })).sort((a, b) => a.sort - b.sort).map(({ value }) => value);
+      displayQuestion();
+    }
+  
+    if (questionNumber === 2) {
+      questionEl.textContent = "2. Commonly used data types DO Not Include: ";
+      answerSet = answer2.map(value => ({ value, sort: Math.random() })).sort((a, b) => a.sort - b.sort).map(({ value }) => value);
+      displayQuestion();
+    }
+  
+    if (questionNumber === 3) {
+  
+      questionEl.textContent = "3. The condition in an if/else statement is enclosed with ______.";
+      answerSet = answer3.map(value => ({ value, sort: Math.random() })).sort((a, b) => a.sort - b.sort).map(({ value }) => value);
+      displayQuestion();
+  
+    }
+  
+    if (questionNumber === 4) {
+      questionEl.textContent = "4. Arrays in JavaScript can be used to store _________ ";
+      answerSet = answer4.map(value => ({ value, sort: Math.random() })).sort((a, b) => a.sort - b.sort).map(({ value }) => value);
+      displayQuestion();
+  
+    }
+  
+    if (questionNumber === 5) {
+      questionEl.textContent = "5. A very useful tool used during development and debugging for printing content to the debugger is:"
+      answerSet = answer5.map(value => ({ value, sort: Math.random() })).sort((a, b) => a.sort - b.sort).map(({ value }) => value);
+      displayQuestion();
+  
+  
+    }
+  
+    if (questionNumber === 6) {
+      questionEl.textContent = "6. What div is this statement accessing document.getElementByID('start-btn');? _______"
+      answerSet = answer6.map(value => ({ value, sort: Math.random() })).sort((a, b) => a.sort - b.sort).map(({ value }) => value);
+      displayQuestion();
+  
+    }
+  }
+  
